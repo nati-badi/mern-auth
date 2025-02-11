3002c01245717058c2fb6eced3593fce085bf7f7
Nati <natibadigojimma@gmail.com>
2025-02-11 20:44:23 +0300

    controller(authController): wrote email verification controller functions

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,9 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
+import transporter from "../config/nodemailer.js";
 
+// Register a new user
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -38,12 +40,23 @@ export const register = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     });
 
+    // Send a welcome email to the user
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Welcome to Badi app!",
+      text: `Hello ${name}, Your account has been created successfully with an email ${email}.`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
 
+// Login a user
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -81,6 +94,7 @@ export const login = async (req, res) => {
   }
 };
 
+// Logout a user
 export const logout = (req, res) => {
   try {
     res.clearCookie("token", {
@@ -90,6 +104,84 @@ export const logout = (req, res) => {
     });
 
     res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Send a verification email to the user
+export const sendVerifyOtp = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    const user = await userModel.findById(userId);
+
+    if (user.isAccountVerified) {
+      return res.status(400).json({ message: "Account already verified" });
+    }
+
+    const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+    user.verifyOtp = otp;
+    user.verifyOtpExpiry = Date.now() + 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
+    await user.save();
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: user.email,
+      subject: "Verify your email",
+      // text: `Hello ${user.name}, please verify your email by entering the OTP ${otp} within 24 hours. If you did not request this, please ignore this email. Thank you!`,
+      html: `<p>Hello ${user.name},</p>
+        <p>Please verify your email by entering the OTP <b>${otp}</b> within <b>24 hours</b>. If you did not request this, please ignore this email. Thank you!</p>
+        <p>Best regards,</p>
+        <p>Badi App</p>`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ message: "Verification email sent successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Verify the user's email
+export const verifyEmail = async (req, res) => {
+  const { userId, otp } = req.body;
+
+  if (!userId || !otp) {
+    return res
+      .status(400)
+      .json({ message: "Please provide all required fields" });
+  }
+
+  try {
+    const user = await userModel.findById(userId);
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid user" });
+    }
+
+    if (user.isAccountVerified) {
+      return res.status(400).json({ message: "Account already verified" });
+    }
+
+    if (user.verifyOtp === "" || user.verifyOtp !== otp) {
+      return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    if (user.verifyOtpExpiry < Date.now()) {
+      return res.status(400).json({ message: "OTP expired" });
+    }
+
+    user.isAccountVerified = true;
+    user.verifyOtp = "";
+    user.verifyOtpExpiry = "";
+
+    await user.save();
+
+    res.status(200).json({ message: "Email verified successfully" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,9 +14,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+// API routes/endpoints
 app.get("/", (req, res) => {
   res.send("Api is running!");
 });
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
