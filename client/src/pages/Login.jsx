0375d8bff71db6cd4b519195ae077bc294d7a879
Nati <natibadigojimma@gmail.com>
2025-02-14 23:54:30 +0300

    feat(login): added login & sign up

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../assets/logo.svg";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [state, setState] = useState("Sign Up");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
+      <img
+        onClick={() => navigate("/")}
+        src={logo}
+        alt="Logo"
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:2-32 cursor-pointer sm:w-16"
+      />
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign Up" ? "Create an Account" : "Login"}
+        </h2>
+
+        <p className="text-center text-sm mb-6">
+          {state === "Sign Up"
+            ? "Sign up with your email"
+            : "Login with your email"}
+        </p>
+
+        <form>
+          {state === "Sign Up" && (
+            <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+              <MdOutlineAccountCircle className="w-5 h-5" />
+              <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                type="text"
+                placeholder="Full Name"
+                required
+                className="bg-transparent outline-none"
+              />
+            </div>
+          )}
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <MdOutlineEmail className="w-5 h-5" />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              value={email}
+              placeholder="Email"
+              required
+              className="bg-transparent outline-none"
+            />
+          </div>
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <RiLockPasswordLine className="w-5 h-5" />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              placeholder="Password"
+              required
+              className="bg-transparent outline-none"
+            />
+          </div>
+          <p
+            onClick={() => navigate("/forgot-password")}
+            className="mb-4 text-indigo-500 cursor-pointer"
+          >
+            Forgot Password?
+          </p>
+          <button className="w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium cursor-pointer">
+            {state}
+          </button>
+        </form>
+
+        {state === "Sign Up" ? (
+          <p className="text-gray-400 text-center text-xm mt-4">
+            Already have an account?{" "}
+            <span
+              className="text-blue-400 cursor-pointer underline"
+              onClick={() => setState("Login")}
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="text-gray-400 text-center text-xm mt-4">
+            Don&apos;t have an account?{" "}
+            <span
+              className="text-blue-400 cursor-pointer underline"
+              onClick={() => setState("Sign Up")}
+            >
+              Sign up here
+            </span>
+          </p>
+        )}
+      </div>
     </div>
   );
 };
