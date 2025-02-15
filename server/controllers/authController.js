d43f2c0bce2c4fbdbaf1e1c775a380397bbcfb9c
Nati <natibadigojimma@gmail.com>
2025-02-15 18:46:16 +0300

    feat(login): tried to integrate front & backend, recommitting

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -88,7 +88,7 @@ export const login = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     });
 
-    res.status(200).json({ message: "Login successful" });
+    res.status(200).json({ success: true, message: "Login successful" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- server/server.js
+++ server/server.js
@@ -11,9 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
+const allowedOrigins = ["http://localhost:5173"];
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true }));
+app.use(cors({ credentials: true, origin: allowedOrigins }));
 
 // API routes/endpoints
 app.get("/", (req, res) => {
